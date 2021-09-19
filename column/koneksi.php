fcb16d9ed714dab8a0e4a19b2760166c6550c3e5
amirsantoso <amir.santosos@gmail.com>
2021-09-19 18:02:53 +0700

    first commit

---
+++ column/koneksi.php
@@ -0,0 +1,5 @@
+<?php
+include "parser-php-version.php"; //Konversi dan migrasi PHP version
+	mysql_connect("localhost","root","A177irs1m918@123") or die ("Koneksi Gagal");
+	mysql_select_db("db_koperasi") or die ("Database Tidak Terakses");
+?>
