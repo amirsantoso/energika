6e90de69106d927424ec317643ba7d469e3691f8
amirsantoso <amir.santosos@gmail.com>
2021-09-19 19:44:25 +0700

    Update koneksi.php

--- column/koneksi.php
+++ column/koneksi.php
@@ -1,5 +1,5 @@
 <?php
 include "parser-php-version.php"; //Konversi dan migrasi PHP version
-	mysql_connect("u-cdbr-west-01.cleardb.com","b08b36868116a6","c7b82c31") or die ("Koneksi Gagal");
+	mysql_connect("eu-cdbr-west-01.cleardb.com","b08b36868116a6","c7b82c31") or die ("Koneksi Gagal");
 	mysql_select_db("heroku_f686e8cfe62ca31") or die ("Database Tidak Terakses");
 ?>
